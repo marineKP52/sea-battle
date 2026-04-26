e86ac0207056f29cbd600e988e9fff6d71fb7ad9
marineKP52 <gls.maryna@gmail.com>
2026-04-26 22:48:08 +0300

    SB-037 done.

--- js/game-utilities.js
+++ js/game-utilities.js
@@ -5,23 +5,26 @@ function createGameElement(tagName, classNames, parent) {
     return newElement;
 }
 
-const board = document.querySelector('.main__board');
-const letters = "ABCDEFGHIJK";
+const boards = document.querySelectorAll('.main__board');
+const letters = "АБВГДЄЖЗИІ";
 
-for (let i = 0; i < 11; i++) {
-    for (let j = 0; j < 11; j++) {
-        let cell = createGameElement('div', 'main__board__cell', board);
+for (let board of boards) {
+    for (let i = 0; i < 11; i++) {
+        for (let j = 0; j < 11; j++) {
+            let cell = createGameElement('div', 'main__board__cell', board);
 
-        if (i === 0) cell.style.borderTop = "none";
-        if (j === 10) cell.style.borderRight = "none";
+            if (i === 0) cell.style.borderTop = "none";
+            if (j === 10) cell.style.borderRight = "none";
 
-        if (i === 0) {
-            cell.textContent = letters[j - 1];
-            cell.classList.add("label");
-        }
-        else if (j === 0) {
-            cell.textContent = i;
-            cell.classList.add("label");
+            if (i === 0) {
+                cell.textContent = letters[j - 1];
+                cell.classList.add("label");
+            }
+            else if (j === 0) {
+                cell.textContent = i;
+                cell.classList.add("label");
+            }
         }
     }
 }
+
