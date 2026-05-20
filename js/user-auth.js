2fffddc94a4102752a06a26d86b9bf80fa634aba
marineKP52 <gls.maryna@gmail.com>
2026-05-20 23:38:28 +0300

    SB-033 another bug fixed.

--- js/user-auth.js
+++ js/user-auth.js
@@ -2,9 +2,8 @@ let user = JSON.parse(localStorage.getItem('userData'));
 
 if (!user) {
     alert("Виникли проблеми під час входу в акаунт.");
-    return;
 }
-
-let usernameField = document.getElementById("username-span");
-
-usernameField.textContent = user.login;
+else {
+    let usernameField = document.getElementById("username-span");
+    usernameField.textContent = user.login;
+}
